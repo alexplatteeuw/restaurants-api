028669f3ca4d3aaf546b7290c62c1a68b7dcef6b
Alex Platteeuw <aplatteeuw@outlook.fr>
2021-01-17 18:30:52 +0100

    save to github

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  acts_as_token_authenticatable
+  has_many :restaurants
+  has_many :comments
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
